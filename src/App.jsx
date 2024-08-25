d9685e4dd012c755f11e3044ca48f0c8e707727b
Ganesh-Paulraja <ganeshsgk555@gmail.com>
2024-08-25 08:17:49 +0530

    props practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Props from './components/Props'
+
 
 function App() {
   return (
-    <div>
-      <h1 className='text-red-900 py-14'>Hello world</h1>
+    <div className='bg-black p-20'>
+      <h1 className='text-red-500 py-14 bg-yellow-500 opacity-80 border rounded'>Hello world</h1>
+      <Props name="myName" work="myWork"/>
     </div>
   )
 }
---
+++ src/components/Props.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Props({name,work}) {
+  return (
+    <div className='text-white'>Props {name}, {work}</div>
+  )
+}
